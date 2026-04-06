3dd6f13c50ebe93c78fdc6df75719a2fbf11f22e
Дмитрий <celysevdima6@gmail.com>
2026-04-06 13:50:45 +0300

    Add files via upload

---
+++ lesson25/main.js
@@ -0,0 +1,61 @@
+// Задача 1.
+// Создайте объект person с несколькими свойствами, содержащими информацию о вас. Затем выведите значения этих свойств в консоль.
+
+const person = {
+  name: "Дима",
+  benchPress: "100кг",
+  height: "187см",
+};
+
+for (const personKey in person) {
+  console.log(person[personKey]);
+}
+
+// Задача 2.
+// Создайте функцию isEmpty, которая проверяет является ли переданный объект пустым. Если объект пуст - верните true, в противном случае false.
+
+function isEmpty(object) {
+  for (const key in object) {
+    return false;
+  }
+  return true;
+}
+console.log(isEmpty(person));
+
+// Задача 3.
+// Создайте объект task с несколькими свойствами: title, description, isCompleted.
+// Напишите функцию cloneAndModify(object, modifications), которая с помощью оператора spread создает копию объекта и применяет изменения из объекта modifications.
+// Затем с помощью цикла for in выведите все свойства полученного объекта.
+
+const task = {
+  title: "я не знаю что писать",
+  description: "здесь тоже не знаю",
+  isCompleted: true,
+};
+
+function cloneAndModify(object, modifications) {
+  const newTask = { ...object, ...modifications };
+
+  for (const KeyNewTask in newTask) {
+    console.log(`${KeyNewTask}: ${newTask[KeyNewTask]}`);
+  }
+}
+
+cloneAndModify(task);
+
+// Задача 4.
+// Создайте функцию callAllMethods, которая принимает объект и вызывает все его методы.
+
+// Пример использования:
+// const myObject = {
+//     method1() {
+//         console.log('Метод 1 вызван');
+//     },
+//     method2() {
+//         console.log('Метод 2 вызван');
+//     },
+//     property: 'Это не метод'
+// };
+// callAllMethods(myObject);
+
+function callAllMethods(object) {}
